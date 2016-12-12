ce2ae5e4c6a8c925508a3f57d31138e5220f7a42
Elliot Thomas <elliotnthomas@gmail.com>
2016-12-12 15:47:32 -0600

    added clear form function

--- public/scripts/cc3.js
+++ public/scripts/cc3.js
@@ -11,20 +11,28 @@ $( document ).ready( function(){
     success: function(response){
       console.log('back from post call:', response);
       for (var i = 0; i < response.length; i++) {
-        $('#outputDiv').append('<strong>' + response[i].whoseJoke + ':' + '</strong>' + '<p>' + response[i].jokeQuestion + '</p>' + '<p>' + response[i].punchLine + '</p>');
+        $('#outputDiv').append('<strong>' + response[i].whoseJoke + ':' + '</strong>' + '<p>' + response[i].jokeQuestion +
+          '</p>' + '<p class ="punchLine">' + response[i].punchLine + '</p>');
       }//end for loop
+      $('.punchLine').hide();
       },
       error: function (){
         console.log('error with ajax call...');
       }
     });//end ajax call
 
+  $('.punchLine').hide();
 
   $( '#addJokeButton' ).on( 'click', function(){
     console.log( 'addJokeButton on click');
     createAndSendNewObject();
     recieveNewJoke();
+    clearForm();
   }); // end addJokeButton on click
+
+  $('#showPunchline').on('click', function(){
+    $('.punchLine').toggle();
+  });//end show punchlines
 }); // end doc ready
 
 function createAndSendNewObject (){
@@ -67,5 +75,12 @@ function recieveNewJoke () {
 };//end recieve joke
 
 function displayNewJoke (array){
-    $('#outputDiv').append('<strong>' + array[array.length - 1].whoseJoke + ':' + '</strong>' + '<p>' + array[array.length - 1].jokeQuestion + '</p>' + '<p>' + array[array.length - 1].punchLine + '</p>');
+    $('#outputDiv').append('<strong>' + array[array.length - 1].whoseJoke + ':' + '</strong>' + '<p>' + array[array.length - 1].jokeQuestion + '</p>' + '<p class="punchLine">' + array[array.length - 1].punchLine + '</p>');
+    $('.punchLine').hide();
 }; //end display joke
+
+function clearForm (){
+  $('#whoseJokeIn').val('');
+  $('#questionIn').val('');
+  $('#punchlineIn').val('');
+}; //end clear form
